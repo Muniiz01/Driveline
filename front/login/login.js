8551697091ba1cef38ab2d9c21282766ad98fd2f
vitorSchuster <157045597+vitorSchuster@users.noreply.github.com>
2024-03-04 16:40:55 -0300

    Tela de login
    a tela de login do usuário/administrador com campo de email e senha estilizados com bootstrap 5 e um pequeno código js para o display da senha.

---
+++ front/login/login.js
@@ -0,0 +1,8 @@
+function mostrarSenha() {  
+  var senha = document.getElementById("senha")
+  if (senha.type === "password") {  
+    senha.type = "text"
+  } else {  
+    senha.type = "password"
+  }  
+}  
