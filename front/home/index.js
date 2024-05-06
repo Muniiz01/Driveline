25b8d6547b18782554963ac4dd616aad982cf299
Vitor luis schuster <157045597+vitorSchuster@users.noreply.github.com>
2024-05-06 16:14:53 -0300

    nova home

---
+++ front/home/index.js
@@ -0,0 +1,74 @@
+document.addEventListener('scroll', rolar)
+//
+var texto1 = document.getElementById('campo_texto1')
+var texto2 = document.getElementById('campo_texto2')
+var navbar = document.getElementById('navbar')
+//
+var ultimaposicao = 1
+var podever = 0
+
+function rolar() {
+
+    var posicaoatual = window.scrollY;
+
+    if (posicaoatual > ultimaposicao && podever === 0) {
+        podever = 1;
+    } else {
+        setInterval(() => {
+            navbar.style.transform = "translateY(0px)"
+        }, 2000);
+        }
+    ultimaposicao = posicaoatual;
+}
+////////////////////////////////////////////////////////////////////
+
+var passa_slide = 0
+function passa() {
+    passa_slide++//acrescenta de 1 em 1 segundos
+    console.log(passa_slide,'passa_slide')
+    ////////////////////////////////////////////////////////////////
+    switch(passa_slide){
+        case 1:
+            texto1.style.opacity = ("1")
+            // texto1.style.transform = ("translateX(0px)")
+            console.log("passou 1, trouxe")
+        break
+        case 11://11
+            texto1.style.opacity = ("0")
+            // texto1.style.transform = ("translateX(-1000px)")
+            console.log("passou 2, escondeu-se")
+        break
+        case 12://12
+        texto2.style.opacity = ("1")
+        // texto2.style.transform = ("translateX(0px)")
+        console.log("passou 3, trouxe")
+        break
+        case 22://22
+            texto2.style.opacity = ("0")
+            // texto2.style.transform = ("translateX(-1000px)") 
+            console.log("passou 4, escondeu-se")
+            passa_slide = 0
+        break
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+var tema_site = 0
+function tema(){
+    tema_site++
+    if(tema_site == 1){
+        document.body.style.setProperty('--fundo', 'rgb(41, 41, 41)');    
+        console.log("fundo escuro")
+    }else{
+        document.body.style.setProperty('--fundo', 'rgb(216, 216, 216)'); 
+        console.log("fundo branco")
+        tema_site = 0
+    }
+    
+}
+
+////////////////////////////////////////////////////////////////////
+
+setInterval(() => {
+    passa()
+}, 1000)//1 segundo
