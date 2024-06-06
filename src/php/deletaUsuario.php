729591ca9ab373970dcd0384442ba677d5d325af
Victor Muniz <victormuniz281@outlook.com>
2024-06-06 16:39:21 -0300

    alterei o popup da barra de pesquisa

--- src/php/deletaUsuario.php
+++ src/php/deletaUsuario.php
@@ -14,9 +14,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if($nivelAcess == 2){
         $sql = "DELETE FROM funcionarios WHERE id_usuario = $idUser";
     
-    }else{
+    }elseif{
         $sql = "DELETE FROM usuarios WHERE id_usuario = $idUser";
 
+    }elseif($nivelAcess == 4){
+        $sql = "DELETE FROM veiculos WHERE id_veiculos = $idCar";
     }
    $resultado = $conn->query($sql);
 
